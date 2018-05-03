04ebd8825471caf87b91636898702eeb35b42173
Kim Ji Youn <uto9806@naver.com>
2018-05-03 11:21:25 +0000

    first commit5월13일

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,20 @@
 class ArticlesController < ApplicationController
+    def create
+        params.permit!
+        @article=Article.new(params[:article])
+        @article.save
+        redirect_to @article
+        
+    end
     def new
     end
     
-    def create
-       params.permit!
-       @article = Article.new(params[:article])
-       @article.save()
-       redirect_to @article
+    def index
+        @articles=Article.all
     end
     
     def show
         @article = Article.find(params[:id])
+    
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  # ggkgk
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  #dfddf
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
